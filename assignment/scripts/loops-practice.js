7c21052e287abb11249a570d2d8436a2d8c33230
Russell Barr <rbarrsquared@gmail.com>
2021-07-27 16:47:23 -0500

    part 2, q3

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,7 +13,7 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
-for ( let i = 0; i <= 5; i++){
+for ( let i = 0; i < 6; i++){
   console.log(i);
 }
 //- I changed the conditional part of the loop.
@@ -21,7 +21,7 @@ for ( let i = 0; i <= 5; i++){
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
-for (let i = 3; i <=5; i++){
+for (let i = 3; i < 6; i++){
   console.log(i);
 }
 // - I changed the starting index variable.
@@ -29,7 +29,7 @@ for (let i = 3; i <=5; i++){
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-for(let i = 2; i <= 10; i++){
+for(let i = 2; i < 11; i++){
   if(i %2===0) {
     console.log(i);
   }
@@ -57,9 +57,22 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
-
+let index = 0;
+while (index < stars.length){
+  console.log(stars[index]);
+  index++;
+}
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
+let index = 0;
+while ( index < 6) {
+  console.log(index);
+  index++
+}
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+let index = 10;
+while ( index > 4) {
+  console.log(index);
+  index--;
+}
